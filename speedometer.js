f1fba0cce2df2b68e3b941c334ee23377cac4ae6
Joao Melo <vivitomelo@gmail.com>
2024-06-14 00:25:16 -0300

    Updating js file

--- speedometer.js
+++ speedometer.js
@@ -2,16 +2,35 @@ const speedElement = document.querySelector("#speed")
 const startBtn = document.querySelector("#start")
 const stopBtn = document.querySelector("#stop")
 
+let watchID = null
 startBtn.addEventListener("click", ()=>{
+    if(watchID)
+        return
 
-    function handleSuccess(position)
-    navigator.geolocation.watchPosition()
+    function handleSuccess(position) {
+        speedElement.innerText = position.coords.speed?
+        (position.coords.speed * 3.6).toFixed(1):0
+    }
+
+    function handleError(position) {
+        console.log(error.msg)
+    }
+
+    const options = {enableHighAccuracy: true} 
+        
+
+    watchID = navigator.geolocation.watchPosition(handleSuccess, handleError, options)
     startBtn.classList.add("d-none")
     stopBtn.classList.remove("d-none")
 })
 
 stopBtn.addEventListener("click", ()=>{
     
+    if(!watchID)
+        return
+    
+    navigator.geolocation.clearWatch(watchID)
+    watchID = null
     stopBtn.classList.add("d-none")
     startBtn.classList.remove("d-none")
 
