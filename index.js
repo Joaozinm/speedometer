574765fa10b51f8345eede03e36883dc7e331511
Joao Melo <vivitomelo@gmail.com>
2024-06-19 01:05:22 -0300

    Ending list backend page

--- index.js
+++ index.js
@@ -25,13 +25,14 @@ allRides.forEach(async ([id, value])=>{
     durationDiv.innerText = getDuration(ride)
 
     const dateDiv = document.createElement("div")
-    date.innerText = getStartDate(ride)
+    dateDiv.innerText = getStartDate(ride)
 
 
     itemElement.appendChild(cityDiv)
     itemElement.appendChild(maxSpeedDiv)
     itemElement.appendChild(distanceDiv)
     itemElement.appendChild(durationDiv)
+    itemElement.appendChild(dateDiv)
 
     rideListElement.appendChild(itemElement)
 
@@ -108,5 +109,15 @@ function getDuration(ride) {
 }
 
 function getStartDate(ride) {
-    const d = new Date()
+    const d = new Date(ride.startTime)
+
+    const day = d.toLocaleString("en-US", {day: "numeric"})
+    const month = d.toLocaleString("en-US", {month: "short"})
+    const year = d.toLocaleString("en-US", {year: "numeric"})
+
+    const hour = d.toLocaleString("en-US", {hour: "2-digit"})
+    const minute = d.toLocaleString("en-US", {minute: "2-digit"})
+
+
+    return `${hour}:${minute} - ${month} ${day}, ${year}`
 }
