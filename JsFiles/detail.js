95401a0973f549868753244f6bb99849ee5437c8
Joao Melo <vivitomelo@gmail.com>
2024-07-08 11:30:25 -0300

    Edit map template

--- JsFiles/detail.js
+++ JsFiles/detail.js
@@ -1,66 +1,61 @@
-    const params = new URLSearchParams(window.location.search)
-    const rideID = params.get("id")
-    const ride = getRideRecord(rideID)
+const params = new URLSearchParams(window.location.search);
+const rideID = params.get("id");
+const ride = getRideRecord(rideID);
 
-    document.addEventListener("DOMContentLoaded", async ()=>{
+document.addEventListener("DOMContentLoaded", async () => {
+  const firstPosition = ride.data[0];
+  const firstLocationData = await getLocationData(
+    firstPosition.latitude,
+    firstPosition.longitude
+  );
 
+  const dataElement = document.createElement("div");
 
+  const cityDiv = document.createElement("div");
+  cityDiv.innerText = `${firstLocationData.city} - ${firstLocationData.countryCode}`;
+  cityDiv.className = "font-h1";
 
+  const maxSpeedDiv = document.createElement("div");
+  maxSpeedDiv.innerText = `Max speed: ${getMaxSpeed(ride.data)} Km/h`;
+  maxSpeedDiv.className = "font-speed";
 
-    const firstPosition = ride.data[0]
-    const firstLocationData = await getLocationData(firstPosition.latitude, firstPosition.longitude)
+  const distanceDiv = document.createElement("div");
+  distanceDiv.innerText = `Distance: ${getDistance(ride.data)}`;
 
+  const durationDiv = document.createElement("div");
+  durationDiv.innerText = getDuration(ride);
 
-    const dataElement = document.createElement("div")
+  const dateDiv = document.createElement("div");
+  dateDiv.innerText = getStartDate(ride);
 
-    const cityDiv = document.createElement("div")
-    cityDiv.innerText= `${firstLocationData.city} - ${firstLocationData.countryCode}`
-    cityDiv.className = "font-h1"
+  dataElement.appendChild(cityDiv);
+  dataElement.appendChild(maxSpeedDiv);
+  dataElement.appendChild(distanceDiv);
+  dataElement.appendChild(durationDiv);
+  dataElement.appendChild(dateDiv);
 
-    const maxSpeedDiv = document.createElement("div")
-    maxSpeedDiv.innerText = `Max speed: ${getMaxSpeed(ride.data)} Km/h`
-    maxSpeedDiv.className = "font-speed"
+  document.querySelector("#data").appendChild(dataElement);
 
-    const distanceDiv = document.createElement("div")
-    distanceDiv.innerText = `Distance: ${getDistance(ride.data)}`
+  const deleteButton = document.querySelector("#deleteBtn");
+  deleteButton.addEventListener("click", () => {
+    deleteRide(rideID);
+    window.location.href = "./";
+  });
 
-    const durationDiv = document.createElement("div")
-    durationDiv.innerText = getDuration(ride)
+  const map = L.map("mapDetail");
+  map.setView([firstPosition.latitude, firstPosition.longitude], 13);
+  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    minZoom: 5,
+    maxZoom: 18,
+    ext: "png",
+  }).addTo(map);
 
-    const dateDiv = document.createElement("div")
-    dateDiv.innerText = getStartDate(ride)
+  const positionsArray = ride.data.map((position) => {
+    return [position.latitude, position.longitude];
+  });
 
+  const polyline = L.polyline(positionsArray, { color: "blue" });
+  polyline.addTo(map);
 
-    dataElement.appendChild(cityDiv)
-    dataElement.appendChild(maxSpeedDiv)
-    dataElement.appendChild(distanceDiv)
-    dataElement.appendChild(durationDiv)
-    dataElement.appendChild(dateDiv)
-
-    document.querySelector("#data").appendChild(dataElement)
-
-    const deleteButton = document.querySelector("#deleteBtn")
-    deleteButton.addEventListener("click", ()=>{
-        
-        deleteRide(rideID)
-        window.location.href = "./"
-
-    })
-
-    const map = L.map("mapDetail")
-    map.setView([firstPosition.latitude, firstPosition.longitude], 13)
-    L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_terrain/{z}/{x}/{y}{r}.{ext}', {
-        minZoom: 5,
-        maxZoom: 18,
-        ext: 'png'
-    }).addTo(map);
-
-    const positionsArray = ride.data.map((position)=>{
-        return [position.latitude, position.longitude]
-    })
-
-    const polyline = L.polyline(positionsArray, {color:"blue"})
-    polyline.addTo(map)
-
-    map.fitBounds(polyline.getBounds())
-})
+  map.fitBounds(polyline.getBounds());
+});
--- JsFiles/index.js
+++ JsFiles/index.js
@@ -1,74 +1,69 @@
-const rideListElement = document.querySelector("#rideList")
-const allRides = getAllRides()
+const rideListElement = document.querySelector("#rideList");
+const allRides = getAllRides();
 
-allRides.forEach(async ([id, value])=>{
-    const ride = JSON.parse(value)
-    ride.id = id
+allRides.forEach(async ([id, value]) => {
+  const ride = JSON.parse(value);
+  ride.id = id;
 
-    const itemElement= document.createElement("li")
-    itemElement.id = ride.id
-    itemElement.className = "d-flex"
-    rideListElement.appendChild(itemElement)
-    
-    itemElement.addEventListener("click", ()=>{
+  const itemElement = document.createElement("li");
+  itemElement.id = ride.id;
+  itemElement.className = "d-flex";
+  rideListElement.appendChild(itemElement);
 
-        window.location.href = `./detail.html?id=${ride.id}`
+  itemElement.addEventListener("click", () => {
+    window.location.href = `./detail.html?id=${ride.id}`;
+  });
 
-    })
+  const firstPosition = ride.data[0];
+  const firstLocationData = await getLocationData(
+    firstPosition.latitude,
+    firstPosition.longitude
+  );
 
+  const mapID = `map${ride.id}`;
+  const mapElement = document.createElement("div");
+  mapElement.id = mapID;
+  mapElement.style = "width:100px;height:100px";
+  mapElement.classList.add;
 
-    const firstPosition = ride.data[0]
-    const firstLocationData = await getLocationData(firstPosition.latitude, firstPosition.longitude)
+  const dataElement = document.createElement("div");
 
+  const cityDiv = document.createElement("div");
+  cityDiv.innerText = `${firstLocationData.city} - ${firstLocationData.countryCode}`;
+  cityDiv.className = "font-h1";
 
-    const mapID = `map${ride.id}`
-    const mapElement = document.createElement("div")
-    mapElement.id = mapID
-    mapElement.style = "width:100px;height:100px"
-    mapElement.classList.add
+  const maxSpeedDiv = document.createElement("div");
+  maxSpeedDiv.innerText = `Max speed: ${getMaxSpeed(ride.data)} Km/h`;
+  maxSpeedDiv.className = "font-speed";
 
-    const dataElement = document.createElement("div")
+  const distanceDiv = document.createElement("div");
+  distanceDiv.innerText = `Distance: ${getDistance(ride.data)}`;
 
-    const cityDiv = document.createElement("div")
-    cityDiv.innerText= `${firstLocationData.city} - ${firstLocationData.countryCode}`
-    cityDiv.className = "font-h1"
+  const durationDiv = document.createElement("div");
+  durationDiv.innerText = getDuration(ride);
 
-    const maxSpeedDiv = document.createElement("div")
-    maxSpeedDiv.innerText = `Max speed: ${getMaxSpeed(ride.data)} Km/h`
-    maxSpeedDiv.className = "font-speed"
+  const dateDiv = document.createElement("div");
+  dateDiv.innerText = getStartDate(ride);
 
-    const distanceDiv = document.createElement("div")
-    distanceDiv.innerText = `Distance: ${getDistance(ride.data)}`
+  dataElement.appendChild(cityDiv);
+  dataElement.appendChild(maxSpeedDiv);
+  dataElement.appendChild(distanceDiv);
+  dataElement.appendChild(durationDiv);
+  dataElement.appendChild(dateDiv);
 
-    const durationDiv = document.createElement("div")
-    durationDiv.innerText = getDuration(ride)
+  itemElement.appendChild(mapElement);
+  itemElement.appendChild(dataElement);
 
-    const dateDiv = document.createElement("div")
-    dateDiv.innerText = getStartDate(ride)
+  const map = L.map(mapID, {
+    attributionControl: false,
+    zoomControl: false,
+  });
+  map.setView([firstPosition.latitude, firstPosition.longitude], 13);
+  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    minZoom: 5,
+    maxZoom: 18,
+    ext: "png",
+  }).addTo(map);
 
-
-    dataElement.appendChild(cityDiv)
-    dataElement.appendChild(maxSpeedDiv)
-    dataElement.appendChild(distanceDiv)
-    dataElement.appendChild(durationDiv)
-    dataElement.appendChild(dateDiv)
-
-    itemElement.appendChild(mapElement)
-    itemElement.appendChild(dataElement)
-
- 
-    const map = L.map(mapID, {
-        attributionControl: false,
-        zoomControl:false
-    })
-    map.setView([firstPosition.latitude, firstPosition.longitude], 13)
-    L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_terrain/{z}/{x}/{y}{r}.{ext}', {
-        minZoom: 5,
-        maxZoom: 18,
-        ext: 'png'
-    }).addTo(map);
-
-
-    L.marker([firstPosition.latitude, firstPosition.longitude]).addTo(map)
-
-})
+  L.marker([firstPosition.latitude, firstPosition.longitude]).addTo(map);
+});
